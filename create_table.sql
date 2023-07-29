3037a5b1d2ffeb22bc86298c9a9beccef01f0286
Sergey Polevich <sergey.polevich@gmail.com>
2023-07-29 22:20:53 +0200

    Changed Clickhouse db Engine tinyLog to MergeTree

--- create_table.sql
+++ create_table.sql
@@ -1,12 +1,7 @@
--- `default`.ping_times definition
-
-CREATE TABLE default.ping_times
-(
-
-    `dt` DateTime,
-
-    `ping_time_ms` UInt16
-)
-ENGINE = TinyLog
-COMMENT 'List of response times and timestamps.\n dt - the time when the packet received.
-\n ping_time_ms - delay between sending and receiving packet in milliseconds';
+CREATE TABLE `default`.ping_times_collect (
+	dt DateTime('Europe/Berlin'),
+	ping_time_ms UInt64,
+	network_name String (30),
+	connection_ok BINARY (1)
+) ENGINE = MergeTree
+ORDER BY dt;
